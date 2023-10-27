5f1dcc26e84dfbc543b878714a4f9600c3d356f9
Vaishnavi Ralegaonkar <ralegaonkarvaishnavi@gmail.com>
2023-10-27 21:02:59 +0530

    v

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,15 @@
-import './App.css';
-import AllRoutes from './routes/Allroutes';
-import MyNavbar from './components/Navbar';
-import Login from './pages/Login';
-import Signup from './pages/Signup';
-import { useState } from 'react';
+import "./App.css";
+import AllRoutes from "./routes/Allroutes";
+import MyNavbar from "./components/Navbar";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import { useState } from "react";
 
 function App() {
-  const[login,setlogin]=useState(true)
+  const [login, setlogin] = useState(true);
   return (
-    <div className='mainhome' >
-<AllRoutes/>
-
-
+    <div className="mainhome">
+      <AllRoutes />
     </div>
   );
 }
